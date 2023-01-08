ce075a4b0b84e391a50fe3bc1d29ef7f322b98d7
usafLhb <youssef.lahbabta@gmail.com>
2023-01-08 19:13:41 +0100

    Add existing file

---
+++ demo-springcloud-streams-kafka/src/main/java/com/sid/demospringcloudstreamskafka/DemoSpringcloudStreamsKafkaApplication.java
@@ -0,0 +1,15 @@
+package com.sid.demospringcloudstreamskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringcloudStreamsKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringcloudStreamsKafkaApplication.class, args);
+
+		System.out.println("Good");
+	}
+
+}
---
+++ demo-springcloud-streams-kafka/src/test/java/com/sid/demospringcloudstreamskafka/DemoSpringcloudStreamsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.demospringcloudstreamskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringcloudStreamsKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
